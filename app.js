ddd1957c31f97d26fcdd6f20f34258364479e189
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-03-01 13:22:45 +0000

    Finished refactor
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- app.js
+++ app.js
@@ -27,9 +27,60 @@ var sabre_dest_url = "https://api.test.sabre.com/v1/lists/top/destinations"
 var sabre_price_url = "https://api.test.sabre.com/v1/shop/flights/fares"
 var sabre_auth = "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-0123456789012345678!123456!0!ABCDEFGHIJKLM!E2E-1"
 
+var activities = [
+    {
+        name: "HOBBIES",
+        options: [
+            {name: "TECH", method: "unimplemented", provider: "none"},
+            {name: "ARTS", method: "unimplemented", provider: "none"}
+        ]
+    }, 
+    {
+        name: "TRAVEL",
+        options: [
+            {name: "FLIGHT", method: "flights", provider: "Sabre"}
+        ]
+    }, 
+    {
+        name: "DINING",
+        options: [
+            {name: "BREAKFAST", method: "unimplemented", provider: "none"},
+            {name: "LUNCH", method: "unimplemented", provider: "none"},
+            {name: "DINNER", method: "unimplemented", provider: "none"},
+            {name: "COFFEE", method: "unimplemented", provider: "none"}
+        ]
+    }, 
+    {
+        name: "FUN",
+        options: [
+            {name: "MOVIES", method: "movies", provider: "Rotten Tomatoes"},
+            {name: "MUSIC", method: "unimplemented", provider: "none"},
+            {name: "NIGHTLIFE", method: "unimplemented", provider: "none"}
+        ]
+    }, 
+    {
+        name: "OUTDOORS",
+        options: [
+            {name: "HIKING", method: "unimplemented", provider: "none"},
+            {name: "SWIMMING", method: "unimplemented", provider: "none"},
+            {name: "PARK", method: "unimplemented", provider: "none"}
+        ]
+    }, 
+    {
+        name: "SPORTS",
+        options: [
+            {name: "FOOTBALL", method: "unimplemented", provider: "none"},
+            {name: "BASEBALL", method: "unimplemented", provider: "none"},
+            {name: "BASKETBALL", method: "unimplemented", provider: "none"},
+            {name: "SOCCER", method: "unimplemented", provider: "none"}
+        ]
+    } 
+];
+
+
 console.log("Starting server");
 
-var socketPool = [];
+var methods = [];
 
 pg.connect(conString, function(err, client, done) {
     if(err) {
@@ -66,11 +117,29 @@ pg.connect(conString, function(err, client, done) {
         });
     });
 
-    app.get('/movies/:fb_id/:limit', function(req, res, next) {
+    app.get('/shake/:fb_id/:course/:fine/:lat/:long', function(req, res, next) {
         var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "movies", "Rotten Tomatoes");
 
-        var limit = req.params["limit"];
+        var course = req.params["course"];
+        var fine = req.params["fine"];
+        var lat = req.params["lat"];
+        var long = req.params["long"];
+
+        var activity = activities[course - 1].options[fine - 1];
+
+        logQuery(fb_id, activity.name, activity.provider);
+
+        methods[activity.method](lat, long, 10, function(response) {
+            res.json(response);
+        });
+
+    });
+            
+    methods["unimplemented"] = function(lat, long, limit, callback) {
+        callback("Method not implemented");
+    }
+
+    methods["movies"] = function(lat, long, limit, callback) {
         var rotten_params = {"country":"us", "limit":limit, "apikey":rotten_api_key };
         
         request({
@@ -90,18 +159,15 @@ pg.connect(conString, function(err, client, done) {
                         rating_rotten: entry.ratings.critics_rating
                     });
                 });
-                res.json(movies);
+                callback(movies);
             }
         });
         
-    });
+    }
 
-    app.get('/theatres/:fb_id/:location/:distance', function(req, res, next) {
-        var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "theatres", "Google");
-
-        var location = req.params["location"];
-        var radius = req.params["distance"];
+    methods["theatres"] = function(lat, long, limit, callback) {
+        var radius = 20000;
+        var location = lat + ',' + long;
 
         var params = {
             "country": "us",
@@ -127,17 +193,14 @@ pg.connect(conString, function(err, client, done) {
                     });
                 });
 
-                res.json(theatres);
+                callback(theatres);
             }
         });
-    });
+    }
         
-    app.get('/airports/:fb_id/:location/:distance', function(req, res, next) {
-        var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "airports", "Google");
-
-        var location = req.params["location"];
-        var radius = req.params["distance"];
+    methods["airports"] = function(lat, long, limit, callback) {
+        var location = lat + ',' + long;
+        var radius = 20000;
 
         var params = {
             "country": "us",
@@ -163,15 +226,12 @@ pg.connect(conString, function(err, client, done) {
                     });
                 });
 
-                res.json(airports);
+                callback(airports);
             }
         });
-    });
+    }
 
-    app.get('/weather/:fb_id/:state/:city', function(req, res, next) {
-        var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "weather", "Weather Underground");
-
+    methods["weather"] = function (lat, long, limit, callback) {
         var state = req.params["state"];
         var city = req.params["city"];
 
@@ -190,17 +250,13 @@ pg.connect(conString, function(err, client, done) {
                     conditions: body.forecast.simpleforecast.forecastday[0].conditions
                 }
 
-                res.json(weather);
+                callback(weather);
             }
         });
-    });
+    }
 
-    app.get('/destinations/:fb_id/:origin/:limit', function(req, res, next) {
-        var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "destinations", "Sabre");
-
-        var origin = req.params["origin"];
-        var limit = req.params["limit"];
+    methods["destinations"] = function (lat, long, limit, callback) {
+        origin = "DFW";
 
         var params = {
             origin: origin,
@@ -229,16 +285,12 @@ pg.connect(conString, function(err, client, done) {
                         code: destination.Destination.DestinationLocation
                     })
                 });
-                res.json(destinations);
+                callback(destinations);
             }
         });
-    });
+    }
 
-    app.get('/prices/:fb_id/:origin/:dest', function(req, res, next) {
-        var fb_id = req.params["fb_id"];
-        logQuery(fb_id, "prices", "Sabre");
-
-        var origin = req.params["origin"];
+    function prices(origin, dest, callback) {
         var dest = req.params["dest"];
 
         var params = {
@@ -257,10 +309,10 @@ pg.connect(conString, function(err, client, done) {
             }
         }, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                res.json(body);
+                callback(body);
             }
         });
-    });
+    }
 
     function getCity(latLong, callback) {
         var params = {
@@ -294,7 +346,6 @@ pg.connect(conString, function(err, client, done) {
 
     io.on('connection', function(socket) {
         logMessage('Dashboard user connected');
-        socketPool.push(socket);
 
         socket.on('users', function(msg) {
             client.query("SELECT COUNT (id) FROM users;", function(err, result) {
