c02a5836f84d28df5bb7796ad6258f3dbd3b0631
Garrett Greenwood <garrettagreenwood@gmail.com>
2015-03-01 02:05:27 +0000

    added weather forecast and theatre search
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ var moment = require('moment');
 
 var rotten_url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?limit=16&country=us"
 var rotten_api_key = "fwb4ugwu5hwajyaj75qubcqz"
-var rotten_params = {"country":"us", "limit":"16", "apikey":rotten_api_key };
 
+var places_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+var places_api_key = "AIzaSyAWaMGTy2TB0HeZxY7sd1LQ4kJsrKA7y7s"
+
+var weather_url = "http://api.wunderground.com/api/e0c4869f7a01914f/forecast/q/"
+var weather_api_key = "e0c4869f7a01914f"
 
 
 console.log("Starting server");
@@ -34,7 +38,9 @@ pg.connect(conString, function(err, client, done) {
 
     });
 
-    app.get('/movies/:id', function(req, res, next) {
+    app.get('/movies/:id/:limit', function(req, res, next) {
+        var limit = req.params["limit"];
+        var rotten_params = {"country":"us", "limit":limit, "apikey":rotten_api_key };
         
         request({
             url: rotten_url,
@@ -42,11 +48,78 @@ pg.connect(conString, function(err, client, done) {
             qs: rotten_params
         }, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                res.json(body);
                 //var entries = body["movies"]
+                movies = []
+                body["movies"].forEach(function(entry) {
+                    movies.push({ 
+                        name: entry.title,
+                        poster: entry.posters.thumbnail,
+                        runtime: entry.runtime,
+                        rating_mpaa: entry.mpaa_rating,
+                        rating_rotten: entry.ratings.critics_rating
+                    });
+                });
+                res.json(movies);
             }
         });
         
+    });
+
+    app.get('/theatres/:id/:location/:distance', function(req, res, next) {
+        var location = req.params["location"];
+        var radius = req.params["distance"];
+
+        var params = {
+            "country": "us",
+            "key": places_api_key,
+            "location": location,
+            "radius": radius,
+            "types": "movie_theater"
+        };
+        
+        request({
+            url: places_url,
+            json: true,
+            qs: params
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                //var entries = body["movies"]
+                theatres = []
+
+                body["results"].forEach(function(theatre) {
+                    theatres.push({
+                        name: theatre.name,
+                        address: theatre.vicinity
+                    });
+                });
+
+                res.json(theatres);
+            }
+        });
+    });
+        
+    app.get('/weather/:id/:state/:city', function(req, res, next) {
+        var state = req.params["state"];
+        var city = req.params["city"];
+
+        var params = {};
+        
+        request({
+            url: weather_url + state + "/" + city + ".json",
+            json: true,
+            qs: params
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                weather = {
+                    high: body.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+                    low: body.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+                    precip: body.forecast.simpleforecast.forecastday[0].pop,
+                    conditions: body.forecast.simpleforecast.forecastday[0].conditions
+                }
+
+                res.json(weather);
+            }
+        });
     });
 
     app.listen(80, function(){
